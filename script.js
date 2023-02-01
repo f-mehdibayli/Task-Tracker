997957f595d21af0ae31ebeb86ce995f98231d41
f-mehdibayli <farid.mehdibayli@gmail.com>
2023-02-01 22:07:54 +0400

    End2

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const buttonAdd=document.querySelector('.add')
-const formElement=document.querySelector('.form')
-
-const space=document.querySelector('.space')
+const inputDiv=document.querySelector('.input-div')
 
 
 
 
 
+console.log(inputDiv)
+let i;
 
+// for(i=0; i<myNodelist.length; i++){
+
+// }
+
+const enterValue=document.querySelector('enter')
+const deleteIcon=document.querySelector('.delete-icon')
+
+deleteIcon.addEventListener('click',event=>{
+  event.preventDefault();
+  console.log(event.currentTarget.parentElement)
+  event.currentTarget.parentElement.remove()
+})
+const inputDivChild=document.querySelector('.input-div-child')
 buttonAdd.addEventListener('click',(event)=>{
     const newBtn=document.createElement('input')
+    const newDiv=document.createElement('div')
+    const newDeleteIcon=document.createElement('img')
+
+    newDiv.classList.add('input-div-new')
+    newDeleteIcon.src='./img/delete_icon.svg'
+    newDeleteIcon.classList.add('creat-delete')
     newBtn.classList.add('new-button')
-    formElement.insertBefore(newBtn, space);
+
+    newDiv.appendChild(newBtn)
+    newDiv.appendChild(newDeleteIcon)
+    inputDiv.insertBefore(newDiv,inputDiv.lastElementChild);
     event.preventDefault();
     
 
