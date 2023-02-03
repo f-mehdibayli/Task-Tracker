903b9ea224bf6919a6e50191dbf0837abffdc7df
f-mehdibayli <farid.mehdibayli@gmail.com>
2023-02-03 23:27:24 +0400

    Son

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
 const inputDiv=document.querySelector('.input-div')
 const btn=document.querySelector('.add')
 
+let sort = 'azalan'
 const deleteIcon = document.querySelector(".delete-icon")
 
 const listIcon=document.querySelector('.list')
-listIcon.addEventListener('click',(e)=>{
-  e.preventDefault();
-  // e.children[0].src='./img/down_arrow_black.svg';
-  console.log(e.children)
- const input=document.querySelectorAll('.enter')
- input.value.sort((a,b)=> a>b  )
-  
 
+listIcon.addEventListener('click',(e)=>{
+  customSort()
+  if (sort === 'azalan') {
+    listIcon.children[0].src='./img/up_arrow_black.svg';
+    sort = 'coxalan'
+  } else {
+    listIcon.children[0].src='./img/down_arrow_black.svg';
+    sort = 'azalan'
+  } 
 })
 
+function customSort() {
+  const sonuncuEl = [...inputDiv.childNodes].find(item => !item.childNodes[0]?.value || item.childNodes[0]?.value === '');
+  const allTodos = document.querySelectorAll('.input-div-child');
+  let allTodosArr = [...allTodos].filter(item => item.children[0].value !== '' );
+  let sortedArr = []
+  if (sort === 'coxalan') {
+    sortedArr = allTodosArr.sort((a, b) => {
+      const aDeyeri = a.children[0].value;
+      const bDeyeri = b.children[0].value;
+      if (aDeyeri > bDeyeri) return -1
+      if (aDeyeri < bDeyeri) return 1
+      return 0
+    })
+  } else {
+    sortedArr = allTodosArr.sort((a, b) => {
+      const aDeyeri = a.children[0].value;
+      const bDeyeri = b.children[0].value;
+      if (aDeyeri > bDeyeri) return 1
+      if (aDeyeri < bDeyeri) return -1
+      return 0
+    })
+  }
+  console.log('sortedArr: ', sortedArr);
+  sortedArr.forEach(element => {
+    inputDiv.insertBefore(element, sonuncuEl)
+  });
+}
 
+listIcon.addEventListener('mouseenter',(e)=>{
+  if (sort === 'azalan') {
+    listIcon.children[0].src='./img/down_arrow_black.svg';
+  } else {
+    listIcon.children[0].src='./img/up_arrow_black.svg';
+  } 
+})
+listIcon.addEventListener('mouseleave',(e)=>{
+  if (sort === 'azalan') {
+    listIcon.children[0].src='./img/down_arrow_gray.jpg';
+  } else {
+    listIcon.children[0].src='./img/up_arrow_gray.svg';
+  } 
+})
 
 const addDeleteFunction = (el) => {
   el.addEventListener('click', (e) => {
@@ -35,7 +79,8 @@ function newElement(e){
   e.preventDefault()
   const newDiv=document.createElement('div')
   newDiv.classList.add("input-div-child")
-  const newInput=document.createElement('input')
+  const newInput=document.createElement('input');
+
   newInput.classList.add("enter");
   newInput.type = "text";
   const newSpan=document.createElement('span');
@@ -48,6 +93,9 @@ function newElement(e){
   newDiv.appendChild(newInput);
   newDiv.appendChild(newSpan);
   inputDiv.appendChild(newDiv)
+  newInput.addEventListener('input', e => {
+    newInput.value = e.target.value
+  })
 }
 
 btn.addEventListener('click',newElement)
